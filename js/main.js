276cd1db5deb9edd67cf856a46c98a6bb189e497
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-02-13 14:11:40 +0530

    design-accordion-menu

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ jQuery(document).ready(function(){
     infinite: true,
     speed: 300,
     slidesToShow: 4,
+    slidesToScroll: 4,
   });
 
+  $('.accordin-title').click(function(){
+    $(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-content').slideToggle()
+    $(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-content').slideUp()
+    $(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-items').toggleClass("active");
+    $(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-items').removeClass("active");
+
+  });
   });
