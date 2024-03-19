df6560c095f6e7ffe018b9da0ecb804460b2a226
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-03-19 18:33:48 +0530

    appying responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function(){
-  $('.our-team-slider-outer-wrapper').slick({
+
+  jQuery('.humberger-icon').click(function(){
+    jQuery('body , html').toggleClass('open-menu');
+  });
+
+  jQuery('.our-team-slider-outer-wrapper').slick({
     dots: true,
     infinite: true,
     speed: 300,
@@ -7,11 +12,11 @@ jQuery(document).ready(function(){
     slidesToScroll: 4,
   });
 
-  $('.accordin-title').dblclick(function(){
-    $(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-content').slideToggle()
-    $(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-content').slideUp()
-    $(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-items').toggleClass("active");
-    $(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-items').removeClass("active");
+  jQuery('.accordin-title').click(function(){
+    jQuery(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-content').slideToggle()
+    jQuery(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-content').slideUp()
+    jQuery(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-items').toggleClass("active");
+    jQuery(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-items').removeClass("active");
 
   });
   });
