ac166e1ff0c0981be13afc0e7c70c6e425aac0f5
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-03-21 09:57:23 +0530

    solving accordiun douts

--- js/main.js
+++ js/main.js
@@ -10,7 +10,34 @@ jQuery(document).ready(function(){
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 425,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+
+    ]
+    
   });
+  
 
   jQuery('.accordin-title').click(function(){
     jQuery(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-content').slideToggle()
@@ -18,5 +45,17 @@ jQuery(document).ready(function(){
     jQuery(this).closest('.top-reasons-accordin-inner-wrapper').find('.accordin-items').toggleClass("active");
     jQuery(this).closest('.top-reasons-accordin-inner-wrapper').siblings().find('.accordin-items').removeClass("active");
 
+
   });
+
+  jQuery('.footer-menu-title').click(function(e){
+    e.preventDefault();
+    jQuery(this).closest('.footer-site .col-layout-left-side .col-layout').find('.footer-listing').slideToggle()
+    jQuery(this).closest('.footer-site .col-layout-left-side .col-layout').siblings().find('.footer-listing').slideUp()
+    jQuery(this).closest('.footer-site .col-layout-left-side .col-layout').find('.footer-menu-title').toggleClass("active");
+    jQuery(this).closest('.footer-site .col-layout-left-side .col-layout').siblings().find('.footer-menu-title').removeClass("active");
+
+  }); 
+
+  
   });
